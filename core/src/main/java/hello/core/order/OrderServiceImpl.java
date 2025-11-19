2035f58beb946846eb46312d8eb421939e6828f2
CHOSSEANG <jng00153@gmail.com>
2025-11-19 21:52:24 +0900

    AppConfig를 이용한 관심사 분리

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,15 +1,19 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-	private final MemberRepository memberReppository = new MemoryMemberRepository();
-	private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+	private final MemberRepository memberReppository;
+	private DiscountPolicy discountPolicy;
+
+	public OrderServiceImpl(MemberRepository memberReppository, DiscountPolicy discountPolicy) {
+		this.memberReppository = memberReppository;
+		this.discountPolicy = discountPolicy;
+	}
 
 	@Override
 	public Order createOrder(Long memberId, String itemName, int itemPrice) {
